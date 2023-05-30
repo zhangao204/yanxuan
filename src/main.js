e3115a3aa6cd430d3409232a5d39bfa7a03213af
shadowsong <ombrechanson@outlook.com>
2023-05-30 13:55:05 +0800

    添加Quasar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {Quasar} from 'quasar'
 import App from './App.vue'
 import router from './router'
 
-
+// App
 import './assets/main.css'
 
-const app = createApp(App)
+// Quasar
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+    .use(router)
+    .use(Quasar, {
+        plugins: {}
+    })
+    .mount('#app')
